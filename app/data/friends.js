4b532d1d4d033249f6d2726761b4e1c920e5dc29
Rafael Galvez <rfgalvez@msn.com>
2019-10-30 19:39:36 -0400

    finally

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+    {
+    "name":"Des",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+      
+    {
+    "name":"Dymen",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,3,1,4,1,2,3,2,5,2]
+    },
+    { 
+    "name":"Bianca",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,4,2,4,2,3,3,4,5,2]
+    }, 
+    {  
+    "name":"Anita",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,4,5,3,4,2,5,3,4,2]
+    },
+    {  
+    "name":"Angie",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[3,2,5,3,4,1,2,2,3,3]
+    },
+];
+
+module.exports = friends;
